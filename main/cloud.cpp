94e9082976b9373143b4c66be96f0d1644d08e2a
Rashmi0804 <81800774+Rashmi0804@users.noreply.github.com>
2023-02-17 08:44:02 +0530

    Create cloud.cpp

---
+++ main/cloud.cpp
@@ -0,0 +1,9 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello! welcome\n";
+  cout << "PES2UG20CS266 successfully built on pipeline \n";
+  return 0;
+}
